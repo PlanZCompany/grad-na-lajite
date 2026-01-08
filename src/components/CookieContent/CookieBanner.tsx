dcba9c02442ad0261acf1ee66c104caeab0c8142
MoniRudashki-dev <s.rudashki@webrika.bg>
2026-01-08 11:09:10 +0200

    fix lint

--- src/components/CookieContent/CookieBanner.tsx
+++ src/components/CookieContent/CookieBanner.tsx
@@ -8,8 +8,6 @@ import cookieConsent from '@/action/cookieConsent'
 const CookieBanner = () => {
   const [isOpen, setIsOpen] = useState(true)
 
-  if (!isOpen) return null
-
   const handleCookieConsent = async () => {
     const cookieConsentValue = await cookieConsent()
 
@@ -71,53 +69,57 @@ const CookieBanner = () => {
   }, [])
 
   return (
-    <div className="fixed bottom-0 left-0 z-50 right-0">
-      <div
-        className="flex flex-col gap-3 bg-purpleLight 
+    <>
+      {isOpen ? (
+        <div className="fixed bottom-0 left-0 z-50 right-0">
+          <div
+            className="flex flex-col gap-3 bg-purpleLight 
       px-4 py-3 md:py-4 text-sm text-slate-50 shadow-2xl md:flex-row md:items-center md:justify-between md:gap-6"
-      >
-        <div className="md:pl-4">
-          <div className="flex items-center mb-1">
-            <GenericImage
-              src="/static/cookie-image.png"
-              alt="cookie"
-              wrapperClassName="w-6 h-6 md:w-8 md:h-8 relative"
-              imageClassName="w-full h-full object-contain"
-              fill={true}
-            ></GenericImage>
-            <h2 className=" text-base font-semibold">Бисквитки</h2>
+          >
+            <div className="md:pl-4">
+              <div className="flex items-center mb-1">
+                <GenericImage
+                  src="/static/cookie-image.png"
+                  alt="cookie"
+                  wrapperClassName="w-6 h-6 md:w-8 md:h-8 relative"
+                  imageClassName="w-full h-full object-contain"
+                  fill={true}
+                ></GenericImage>
+                <h2 className=" text-base font-semibold">Бисквитки</h2>
+              </div>
+
+              <p className="text-primaryYellow">
+                В „Град на Лъжите“ използваме бисквитки, за да пазим тайните ти в безопасност и да
+                направим преживяването по-добро. <br /> Някои са нужни, за да работи сайтът, а други
+                помагат да разберем как го използваш.{' '}
+                <Link href="/cookie-policy" className="w-full text-white text-center">
+                  <span className="font-georgia font-[400] text-white text-[14px] underline hover:opacity-80">
+                    Политика за бисквитки
+                  </span>
+                </Link>
+              </p>
+            </div>
+
+            <div className="flex flex-col items-center gap-2 justify-center">
+              <GenericButton
+                type="button"
+                styleClass="!text-[12px] !px-2 !py-1"
+                click={() => handleAccept()}
+              >
+                <p className="text-white">Приемам всички</p>
+              </GenericButton>
+              <button
+                type="button"
+                className="underline text-white hover:opacity-80"
+                onClick={() => handleAcceptNecessary()}
+              >
+                <p className="underline">Само нужните</p>
+              </button>
+            </div>
           </div>
-
-          <p className="text-primaryYellow">
-            В „Град на Лъжите“ използваме бисквитки, за да пазим тайните ти в безопасност и да
-            направим преживяването по-добро. <br /> Някои са нужни, за да работи сайтът, а други
-            помагат да разберем как го използваш.{' '}
-            <Link href="/cookie-policy" className="w-full text-white text-center">
-              <span className="font-georgia font-[400] text-white text-[14px] underline hover:opacity-80">
-                Политика за бисквитки
-              </span>
-            </Link>
-          </p>
         </div>
-
-        <div className="flex flex-col items-center gap-2 justify-center">
-          <GenericButton
-            type="button"
-            styleClass="!text-[12px] !px-2 !py-1"
-            click={() => handleAccept()}
-          >
-            <p className="text-white">Приемам всички</p>
-          </GenericButton>
-          <button
-            type="button"
-            className="underline text-white hover:opacity-80"
-            onClick={() => handleAcceptNecessary()}
-          >
-            <p className="underline">Само нужните</p>
-          </button>
-        </div>
-      </div>
-    </div>
+      ) : null}
+    </>
   )
 }
 
